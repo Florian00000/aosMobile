ad6e0710395c5b1dcd78a0d54723f0cb03860ba5
FlorianD <floriandelafosse@gmail.com>
2025-08-28 14:27:26 +0200

    app.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>test</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default App;
